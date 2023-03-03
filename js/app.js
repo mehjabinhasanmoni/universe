630cf308d73582ba14666446ffccfba0411b5dab
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-03-03 21:07:19 +0600

    Display card without Data from API

--- js/app.js
+++ js/app.js
@@ -2,12 +2,31 @@ const loadUniverseAi = async() =>{
     const url = `https://openapi.programming-hero.com/api/ai/tools `
     const res = await fetch(url);
     const data = await res.json();
-    displayAiHub(data.data);
+    displayAiHubs(data.data.tools);
 
 }
-const displayAiHub = aiHub =>{
-    console.log(aiHub);
-}
+const displayAiHubs = aiHubs =>{
+    const aiContainer = document.getElementById('ai-container');
+    aiHubs.forEach(aiHub =>{
+        const aiHubDiv = document.createElement('div');
+        aiHubDiv.classList.add('col');
+        aiHubDiv.innerHTML = `
+        <div class="card h-100">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+        </div>
+        `;
+        aiContainer.appendChild(aiHubDiv);
+        
+    })
+
+};
 
 
 loadUniverseAi();
