965b94deb49dd032703ee10f986f200a8d27c705
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-03-04 01:08:32 +0600

    Loader section added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 const loadUniverseAi = async() =>{
-    const url = `https://openapi.programming-hero.com/api/ai/tools `
+    const loading = document.getElementById('loader');
+    loading.style.display = 'flex';
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
+
     const data = await res.json();
-    displayAiHubs(data.data.tools);
+    console.log("data",data);
+    if(data.status){
+        loading.style.display = 'none';
+        displayAiHubs(data.data.tools);
+    }
+
+   
 
 }
 const displayAiHubs = aiHubs =>{
@@ -11,15 +20,16 @@ const displayAiHubs = aiHubs =>{
         const aiHubDiv = document.createElement('div');
         aiHubDiv.classList.add('col');
         aiHubDiv.innerHTML = `
-        <div class="card h-100">
-        <img src="${aiHub.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
+        <div class="card h-100 p-4">
+            <img src="${aiHub.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Features</h5>
+                forEach
+            </div>
+            <div class="card-footer">
+            <h5 class="card-title">${aiHub.name}</h5>
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
         </div>
         `;
         aiContainer.appendChild(aiHubDiv);
