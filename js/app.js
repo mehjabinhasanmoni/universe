804275f3af1590ec86ffff1439ea426eda5ae2eb
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-03-04 02:02:54 +0600

    Card Feature section and card footer section complete

--- js/app.js
+++ js/app.js
@@ -1,42 +1,43 @@
-const loadUniverseAi = async() =>{
-    const loading = document.getElementById('loader');
-    loading.style.display = 'flex';
-    const url = `https://openapi.programming-hero.com/api/ai/tools`
-    const res = await fetch(url);
+const loadUniverseAi = async () => {
+  const loading = document.getElementById("loader");
+  loading.style.display = "flex";
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  const res = await fetch(url);
 
-    const data = await res.json();
-    console.log("data",data);
-    if(data.status){
-        loading.style.display = 'none';
-        displayAiHubs(data.data.tools);
-    }
-
-   
-
-}
-const displayAiHubs = aiHubs =>{
-    const aiContainer = document.getElementById('ai-container');
-    aiHubs.forEach(aiHub =>{
-        const aiHubDiv = document.createElement('div');
-        aiHubDiv.classList.add('col');
-        aiHubDiv.innerHTML = `
+  const data = await res.json();
+  console.log("data", data);
+  if (data.status) {
+    loading.style.display = "none";
+    displayAiHubs(data.data.tools);
+  }
+};
+const displayAiHubs = (aiHubs) => {
+  const aiContainer = document.getElementById("ai-container");
+  aiHubs.forEach((aiHub) => {
+    const aiHubDiv = document.createElement("div");
+    aiHubDiv.classList.add("col");
+    aiHubDiv.innerHTML = `
         <div class="card h-100 p-4">
             <img src="${aiHub.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Features</h5>
-                forEach
+                <h4 class="card-title">Features</h4>
+                <ol class="m-0 ps-4">
+                ${aiHub.features
+                  .map((feature) => `<li class="my-2"> ${feature}</li>`)
+                  .join("")}
+                 </ol>
             </div>
-            <div class="card-footer">
-            <h5 class="card-title">${aiHub.name}</h5>
-                <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card-footer custom-card-footer d-flex align-items-center justify-content-between">
+                <div>
+                    <h5 class="card-title ">${aiHub.name}</h5>
+                    <i class="fa fa-calendar"></i> ${aiHub.published_in}
+                </div>
+                <i class="footer-right-arrow fa-solid fa-arrow-right-long"></i>
             </div>
         </div>
         `;
-        aiContainer.appendChild(aiHubDiv);
-        
-    })
-
+    aiContainer.appendChild(aiHubDiv);
+  });
 };
-
 
 loadUniverseAi();
