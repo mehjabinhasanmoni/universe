ed117b78e93c8c6723bb259ce326cf7b16954cc2
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-03-03 21:10:42 +0600

    Display Image in card

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const displayAiHubs = aiHubs =>{
         aiHubDiv.classList.add('col');
         aiHubDiv.innerHTML = `
         <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${aiHub.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
